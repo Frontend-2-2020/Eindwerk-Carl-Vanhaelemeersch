5eb28545c20873ab41755715db497713b5d58afc
Carl_Vanhaelemeersch <carl.vanhaelemeersch@gmail.com>
2020-04-22 14:40:07 +0200

    Remove test, bump Register

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,16 +1,24 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Formik } from "formik";
+import { Formik, Form, Field } from "formik";
 
 class Register extends Component {
   render() {
     return (
       <div className="container">
-        <Formik>
-          <div>
-            "first_name" "last_name" "email" "password" "favorite_color"
-            "avatar"
-          </div>
+        <Formik
+          onSubmit={() => console.log("submit")}
+          initialValues={{ first_name: "Voornaam" }}
+        >
+          <Form>
+            <Field type="text" name="first_name" id="first_name"></Field>
+            <Field type="text" name="last_name" id="last_name"></Field>
+            <Field type="email" name="email" id="email"></Field>
+            <Field type="color" name="color" id="color"></Field>
+            <Field type="file" name="avatar" id="avatar"></Field>
+            {/* "first_name" "last_name" "email" "password" "favorite_color"
+            "avatar" */}
+          </Form>
         </Formik>
       </div>
     );
--- src/test.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class test extends Component {
-  render() {
-    return <div>test</div>;
-  }
-}
-
-export default test;
